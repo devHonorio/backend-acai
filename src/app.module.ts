c537b1dd599fc5c725a981fa8481b58a1d8ea1fe
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-02-21 15:04:15 -0300

    feat: create endpoint / return ok: true

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CupsModule } from './cups/cups.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CupsModule, UsersModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule {}
